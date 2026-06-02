cf4bc993c7c3f8b2d0c382b4c690fb068468abed
danielcepatterson <danielcepatterson@gmail.com>
2026-06-01 20:36:36 -0400

    Remove unused code and clean up the App component

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -161,19 +161,6 @@ function App() {
 			</footer>
 		</>
 	);
-							.then((data) => setName(data.name));
-					}}
-					aria-label="get name"
-				>
-					Name from API is: {name}
-				</button>
-				<p>
-					Edit <code>worker/index.ts</code> to change the name
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the logos to learn more</p>
-		</>
-	);
 }
 
 export default App;
